52d7eb2584665062da9e0ed310bbe58c11461ff5
licheng <2501093733@qq.com>
2023-10-13 16:53:41 +0800

    添加SpringBoot依赖!

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,6 +10,10 @@ repositories {
 }
 
 dependencies {
-
+    // 等同于maven中的 <dependencyManagement>...</dependencyManagement> 标签
+    constraints {
+        implementation("org.springframework.boot:spring-boot-dependencies:2.7.16")
+        implementation("org.springframework.boot:spring-boot-starter-web:2.7.16")
+    }
 }
 
--- client/build.gradle.kts
+++ client/build.gradle.kts
@@ -10,10 +10,7 @@ repositories {
 }
 
 dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.9.1"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
+    // spring-boot-web
+    implementation("org.springframework.boot:spring-boot-starter-web:2.7.16")
 }
 
-tasks.test {
-    useJUnitPlatform()
-}
