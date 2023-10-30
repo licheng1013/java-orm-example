6b8382360b23c2fce467f40e31f8be2ba5cdebac
licheng <2501093733@qq.com>
2023-10-30 11:32:14 +0800

    优化功能描述!

--- admin/src/main/java/com/demo/controller/AdminController.java
+++ admin/src/main/java/com/demo/controller/AdminController.java
@@ -4,11 +4,14 @@ import com.demo.common.PassToken;
 import com.demo.common.R;
 import com.demo.entity.Admin;
 import com.demo.service.AdminService;
+import org.apache.ibatis.annotations.Param;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * 注释
@@ -28,10 +31,16 @@ public class AdminController {
     }
 
 
+
+    /**
+     * @Param(userName=aa,password=aaabbaa) 
+     * @Param()
+     */
     @PassToken
     @GetMapping("/index")
-    public Object index(){
-        return R.okMsg("Hello World");
+    public Object index(HttpServletRequest request,Admin admin){
+        System.out.println(admin);
+        return R.okData("Hello World" + request.getHeader("token") + admin.getUserName());
     }
 
 
