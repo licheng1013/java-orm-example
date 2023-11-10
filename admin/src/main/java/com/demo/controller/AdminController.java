5128ebe046ffcfbdbd1f6fd890670b78d3127308
licheng <2501093733@qq.com>
2023-11-10 11:18:24 +0800

    增加自动时间注入功能!

--- admin/src/main/java/com/demo/controller/AdminController.java
+++ admin/src/main/java/com/demo/controller/AdminController.java
@@ -1,6 +1,6 @@
 package com.demo.controller;
 
-import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.demo.common.PassToken;
 import com.demo.common.R;
@@ -8,7 +8,10 @@ import com.demo.dto.PageDto;
 import com.demo.entity.Admin;
 import com.demo.service.AdminService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
 
@@ -37,7 +40,7 @@ public class AdminController {
     @PassToken
     @GetMapping("/list") //分页
     public Object list(Page<Admin> page, Admin v) {
-        return R.okData(PageDto.warp(vService.page(page, new QueryWrapper<>(v))));
+        return R.okData(PageDto.warp(vService.page(page, new LambdaQueryWrapper<>(v).orderByDesc(Admin::getCreateTime))));
     }
 
     @PassToken
